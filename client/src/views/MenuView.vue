7d1f1a242fdd9bef4b247aaf445a68710081f661
MatrixyMN <mn.matrixneo@gmail.com>
2026-09-21 14:31:19 +0800

    feat: display frop down of categories

--- client/src/views/MenuView.vue
+++ client/src/views/MenuView.vue
@@ -14,7 +14,22 @@ const cartItems = ref([]);
 const STORAGE_KEY = "cart"
 
 // TODO: Fetch categories when the component is created
+onMounted (async () => {
+    let url = "http://127.0.0.1:3000/categories"
 
+    try {
+        // axios code must be inside try/catch block
+        let response = await axios.get(url)
+
+        console.log(response.data)
+
+        categories.value = response.data
+
+    } catch (e) {
+        // to display the error message when there is a connection error
+        console.log(e.message)
+    }
+})
 
 // TODO: Fetch items for the currently selected category
 async function getItems() {
@@ -50,7 +65,7 @@ function doAddToCart(itemsToAdd) {
     <!-- TODO: Category selection dropdown -->
     <label for="categories">Categories</label>
     <select class="form-control" id="categories" >
-        <option> category </option>
+        <option v-for="category in categories">{{ category }}</option>
     </select>
     <br>
 
